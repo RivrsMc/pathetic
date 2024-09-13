dd56cf92ee1ef6b37972ccca995e7ca098025dd9
Roch Blondiaux <contact@roch-blondiaux.com>
2024-09-13 13:32:22 +0200

    feat: removed nms modules

--- pathetic-nms/src/main/java/org/patheloper/provider/ChunkDataProviderResolver.java
+++ pathetic-nms/src/main/java/org/patheloper/provider/ChunkDataProviderResolver.java
@@ -1,14 +1,10 @@
 package org.patheloper.provider;
 
+import org.patheloper.api.snapshot.ChunkDataProvider;
+import org.patheloper.provider.paper.PaperChunkDataProvider;
+
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
-import org.patheloper.api.snapshot.ChunkDataProvider;
-import org.patheloper.provider.v1_16.v1_16ChunkDataProviderImpl;
-import org.patheloper.provider.v1_12.v1_12ChunkDataProviderImpl;
-import org.patheloper.provider.v1_15.v1_15ChunkDataProviderImpl;
-import org.patheloper.provider.v1_17.v1_17ChunkDataProviderImpl;
-import org.patheloper.provider.v1_8.v1_8ChunkDataProviderImpl;
-import org.patheloper.provider.paper.PaperChunkDataProvider;
 
 @Getter
 @Slf4j
@@ -22,41 +18,12 @@ public class ChunkDataProviderResolver {
     if (isPaper()) {
       chunkDataProvider = new PaperChunkDataProvider();
     } else {
-      chunkDataProvider = determineChunkDataProvider(major, minor);
+      log.warn("Unsupported server software, please use Paper.");
+      throw new IllegalStateException("Unsupported version: " + version);
     }
 
     log.debug(
-        "Detected version v{}, using {}", version, chunkDataProvider.getClass().getSimpleName());
-  }
-
-  private ChunkDataProvider determineChunkDataProvider(int major, int minor) {
-    final ChunkDataProvider chunkDataProvider;
-    switch (major) {
-      case 21:
-      case 20:
-      case 19:
-      case 18:
-        chunkDataProvider = new v1_18UpChunkDataProviderImpl();
-        break;
-      case 17:
-        chunkDataProvider = new v1_17ChunkDataProviderImpl();
-        break;
-      case 16:
-        chunkDataProvider = new v1_16ChunkDataProviderImpl();
-        break;
-      case 15:
-        chunkDataProvider = new v1_15ChunkDataProviderImpl();
-        break;
-      case 12:
-        chunkDataProvider = new v1_12ChunkDataProviderImpl();
-        break;
-      case 8:
-        chunkDataProvider = new v1_8ChunkDataProviderImpl();
-        break;
-      default:
-        throw new IllegalArgumentException("Unsupported version: " + major + "." + minor);
-    }
-    return chunkDataProvider;
+      "Detected version v{}, using {}", version, chunkDataProvider.getClass().getSimpleName());
   }
 
   private boolean isPaper() {
